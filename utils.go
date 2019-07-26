169fa291520d9fbacffafaad8c791677e9a9ae92
yy <1023767856@qq.com>
2019-07-26 16:35:36 +0800

    change the go.mod

--- utils.go
+++ utils.go
@@ -5,6 +5,6 @@
 package go_helper
 
 // 根据页码和 每页大小获取 对应的开始位置(用以sql的offset 或者 limit)
-func getStartPos(page int, size int) int {
+func GetStartPos(page int, size int) int {
 	return (page - 1) * size
 }
